5301e44ca78e008d4ea7ea8af85951b384258e7f
Ghifani <ghifani03@gmail.com>
2025-08-23 10:48:06 +0700

    form

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Button({text=false, style, onClick, icon=false}) {
+export default function Button({type, text=false, style, onClick, icon=false}) {
     return (
-        <button onClick={onClick} className={`${style}`} >
+        <button type={type} onClick={onClick} className={`${style}`} >
             {icon && <span className='material-icons'>{icon}</span>}
             {text && <span className='text-sm font-semibold'>{text}</span>}
         </button>
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import Button from "../Button";
+import { getInitialData, showFormattedDate } from "../../utils";
 
 const customStyles = {
   content: {
@@ -18,15 +19,51 @@ Modal.setAppElement("#root");
 export default function ModalComponent() {
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [notes, setNotes] = React.useState(getInitialData());
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const MAXCHAR = 50
 
-  function openModal() {
-    setIsOpen(true);
+  function handleTitleChange(event) {
+    if(event.target.value.length <= MAXCHAR) 
+    setTitle(event.target.value);
+  }
+
+  function handleContentChange(event) {
+    setContent(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const newNote = {
+        id: notes.length + 1,
+        title,
+        body: content,
+        createdAt: showFormattedDate(new Date()),
+        archived: false,
+      }
+
+      console.log('data', newNote)
+      setNotes([...notes, newNote])
+    } catch (error) {
+      console.error("Error saving note:", error);
+    } finally {
+      setTitle("")
+      setContent("")
+      setIsOpen(false)
+    }
   }
 
   function afterOpenModal() {
     subtitle.style.color = "#000000";
     subtitle.style.fontSize = "24px";
     subtitle.style.fontWeight = "bold";
+  }
+
+  function openModal() {
+    setIsOpen(true);
   }
 
   function closeModal() {
@@ -46,11 +83,11 @@ export default function ModalComponent() {
       >
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Buat Catatan</h2>
         <div className="flex flex-col items-end gap-3" >
-          <p className="text-sm text-slate-500" >Sisa karakter: 500</p>
-          <form className="w-[400px]" >
-            <input className="w-full border px-2 py-1 rounded-sm" placeholder="Ini adalah judul..." />
-            <textarea className="w-full min-h-32 border mt-4 px-2 py-1 rounded-sm" placeholder="Ini adalah isi catatan..." />
-            <Button text="Simpan" style='bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors cursor-pointer mt-4' onClick={closeModal} />
+          <p className="text-sm text-slate-500" >Sisa karakter: {MAXCHAR - title.length}</p>
+          <form onSubmit={handleSubmit} className="w-[400px]" >
+            <input value={title} onChange={(event) => handleTitleChange(event)} className="w-full border px-2 py-1 rounded-sm" placeholder="Ini adalah judul..." required />
+            <textarea value={content} onChange={(event) => handleContentChange(event)} className="w-full min-h-32 border mt-4 px-2 py-1 rounded-sm" placeholder="Ini adalah isi catatan..." required />
+            <Button type="submit" text="Simpan" style='bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors cursor-pointer mt-4'  />
           </form>
         </div>
       </Modal>
